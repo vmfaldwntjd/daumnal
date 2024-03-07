9a908ee234031a71c23551a6dd9b6c7c7dfdc494
김예지 <ilice0403@naver.com>
2024-03-07 10:16:23 +0900

    feat: Entity 부모클래스 BaseEntity 생성

---
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/entity/BaseEntity.java
@@ -0,0 +1,33 @@
+package com.ssafy.daumnal.global.entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private String createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private String updatedAt;
+
+    @PrePersist
+    void onPrePersist() {
+        this.createdAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        this.updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    void onPreUpdate() {
+        this.updatedAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+}
