6b01c54c35a5d354bd6c94cba13c48eab7989d6b
김예지 <ilice0403@naver.com>
2024-03-07 10:26:46 +0900

    feat: 예외클래스  ExistException과 NoExistException 생성

--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/ErrorCode.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/ErrorCode.java
@@ -10,6 +10,9 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 @Getter
 public enum ErrorCode {
 
+    //일반
+    NOT_EXISTS_ID(BAD_REQUEST, "존재하지 않는 id 입니다."),
+
     //회원
     NOT_EXISTS_MEMBER_ID(BAD_REQUEST, "존재하지 않는 회원 id 입니다.");
 
