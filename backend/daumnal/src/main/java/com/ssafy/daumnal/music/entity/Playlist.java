60fc9881e635fb5fed45a0d406254730473b9373
김예지 <ilice0403@naver.com>
2024-03-07 17:27:30 +0900

    feat: Playlist 엔티티 id 외 컬럼 작성

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/entity/Playlist.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/entity/Playlist.java
@@ -1,8 +1,12 @@
 package com.ssafy.daumnal.music.entity;
 
 import com.ssafy.daumnal.global.entity.BaseEntity;
+import com.ssafy.daumnal.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Getter
@@ -13,4 +17,16 @@ public class Playlist extends BaseEntity {
     @Column(name = "id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "cover_url")
+    @ColumnDefault("https://daumnal.s3.ap-northeast-2.amazonaws.com/playlistCover/basic_cover.jpg")
+    private String coverUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member member;
 }
