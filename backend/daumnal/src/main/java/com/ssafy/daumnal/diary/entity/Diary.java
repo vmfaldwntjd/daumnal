c14f7cb583d47834210b8b4962b50a07c5669f76
김예지 <ilice0403@naver.com>
2024-03-07 15:47:56 +0900

    feat: Member, MemberStatus, SocialProvider 생성
    - Member 엔티티
      - id 외 다른 컬럼 추가
    - MemberStatus Enum 생성
    - SocialProvider Enum 생성
    - Diary 컬럼 일부 추가

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/entity/Diary.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/entity/Diary.java
@@ -13,4 +13,19 @@ public class Diary extends BaseEntity {
     @Column(name = "id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "hash_tag")
+    private String hashTag;
+
+    @Column(name = "photo_url")
+    private String photoUrl;
+
+    @Column(name = "lyrics_line_number")
+    private String lyricsLineNumber;
 }
