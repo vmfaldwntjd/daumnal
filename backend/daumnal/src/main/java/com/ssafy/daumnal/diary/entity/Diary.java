76a0dddf187455ec77d9a2c821838b2bb9b10fcb
김예지 <ilice0403@naver.com>
2024-03-07 16:37:03 +0900

    feat: Diary 엔티티 생성

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/entity/Diary.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/entity/Diary.java
@@ -1,8 +1,13 @@
 package com.ssafy.daumnal.diary.entity;
 
+import com.ssafy.daumnal.emotion.entity.Emotion;
 import com.ssafy.daumnal.global.entity.BaseEntity;
+import com.ssafy.daumnal.member.entity.Member;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Getter
@@ -14,13 +19,19 @@ public class Diary extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "music_id")
+    private Long musicId;
+
     @Column(name = "title", nullable = false)
+    @Size(max = 30)
     private String title;
 
     @Column(name = "content", nullable = false)
+    @Size(max = 3000)
     private String content;
 
     @Column(name = "hash_tag")
+    @Size(max = 30)
     private String hashTag;
 
     @Column(name = "photo_url")
@@ -28,4 +39,14 @@ public class Diary extends BaseEntity {
 
     @Column(name = "lyrics_line_number")
     private String lyricsLineNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member member;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "emotion_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Emotion emotion;
 }
