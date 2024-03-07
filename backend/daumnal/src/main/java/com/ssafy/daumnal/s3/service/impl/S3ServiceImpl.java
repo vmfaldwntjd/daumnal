f28bc061a9d9c8a970da283f9bb8f6c8185dfa33
김예지 <ilice0403@naver.com>
2024-03-07 17:36:42 +0900

    feat: S3ServiceImpl 플레이리스트 커버 업로드 메소드 생성

--- backend/daumnal/src/main/java/com/ssafy/daumnal/s3/service/impl/S3ServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/s3/service/impl/S3ServiceImpl.java
@@ -25,7 +25,7 @@ public class S3ServiceImpl implements S3Service {
      * s3에 일기 내용 중 사진 업로드
      *
      * @param diaryPhotoFile 업로드할 일기 사진 파일
-     * @param diary 일기 사진이 저장되어있을 일기 인스턴스
+     * @param diary 사진을 저장할 일기 인스턴스
      * @return
      */
     @Override
@@ -41,8 +41,23 @@ public class S3ServiceImpl implements S3Service {
         }
     }
 
+    /**
+     * s3에 플레이리스트 커버 업로드
+     *
+     * @param playlistCoverFile 업로드할 플레이리스트 커버 파일
+     * @param playlist 커버를 저장할 플레이리스트 인스턴스
+     * @return
+     */
     @Override
     public String uploadPlaylistCover(MultipartFile playlistCoverFile, Playlist playlist) {
-        return null;
+        try {
+            if (playlist.getCoverUrl() != null && playlist.getCoverUrl().contains(String.format("https://%s.s3.%s.amazonaws.com/", bucket, region))) {
+                delete(playlist.getCoverUrl());
+            }
+
+            return upload(playlistCoverFile, "playlistCover");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
