a7dc769875db4acdf85c622a7151d8e72bb81958
김예지 <ilice0403@naver.com>
2024-03-07 17:00:15 +0900

    feat: Music 엔티티 id 외 컬럼 작성

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/entity/Music.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/entity/Music.java
@@ -1,8 +1,12 @@
 package com.ssafy.daumnal.music.entity;
 
+import com.ssafy.daumnal.emotion.entity.Emotion;
 import com.ssafy.daumnal.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Getter
@@ -13,4 +17,29 @@ public class Music extends BaseEntity {
     @Column(name = "id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "youtube_id", nullable = false)
+    private String youtubeId;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "lyrics", nullable = false)
+    private String lyrics;
+
+    @Column(name = "singer_name", nullable = false)
+    private String singerName;
+
+    @Column(name = "cover_url")
+    @ColumnDefault("https://daumnal.s3.ap-northeast-2.amazonaws.com/musicCover/basic_cover.jpg")
+    private String coverUrl;
+
+    @Column(name = "category")
+    @Enumerated(EnumType.STRING)
+    private String category;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "emotion_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Emotion emotion;
 }
