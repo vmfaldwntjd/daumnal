f7aa5db660947cf80336960e3290c96c608b8b97
김예지 <ilice0403@naver.com>
2024-03-07 15:29:25 +0900

    feat: S3Service, S3ServiceImpl 생성

--- backend/daumnal/src/main/java/com/ssafy/daumnal/s3/controller/S3Controller.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/s3/controller/S3Controller.java
@@ -17,17 +17,17 @@ public class S3Controller {
 
     private final S3Service s3Service;
 
-    @PostMapping("/upload/diaryPhoto")
-    public ApiResponse<?> uploadDiaryPhoto(@RequestParam MultipartFile diaryPhotoFile) {
-        String diaryPhotoUrl = s3Service.uploadDiaryPhoto(diaryPhotoFile);
-
-        return ApiResponse.success(SuccessCode.UPLOAD_DIARY_PHOTO, diaryPhotoUrl);
-    }
-
-    @PostMapping("/upload/playlistCover")
-    public ApiResponse<?> uploadPlaylistCover(@RequestParam MultipartFile playlistCoverFile) {
-        String playlistCoverUrl = s3Service.uploadPlaylistCover(playlistCoverFile);
-
-        return ApiResponse.success(SuccessCode.UPLOAD_PLAYLIST_COVER, playlistCoverUrl);
-    }
+//    @PostMapping("/upload/diaryPhoto")
+//    public ApiResponse<?> uploadDiaryPhoto(@RequestParam MultipartFile diaryPhotoFile) {
+//        String diaryPhotoUrl = s3Service.uploadDiaryPhoto(diaryPhotoFile);
+//
+//        return ApiResponse.success(SuccessCode.UPLOAD_DIARY_PHOTO, diaryPhotoUrl);
+//    }
+//
+//    @PostMapping("/upload/playlistCover")
+//    public ApiResponse<?> uploadPlaylistCover(@RequestParam MultipartFile playlistCoverFile) {
+//        String playlistCoverUrl = s3Service.uploadPlaylistCover(playlistCoverFile);
+//
+//        return ApiResponse.success(SuccessCode.UPLOAD_PLAYLIST_COVER, playlistCoverUrl);
+//    }
 }
