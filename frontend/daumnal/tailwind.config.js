f0b2be738ecc22ee4c4f67c1bab33a635ac82aa8
jojoo <csh990720@gmail.com>
2024-03-07 17:23:58 +0900

    chore: .tsx 파일에 export 추가/ tailwind 색상 코드 추가

--- frontend/daumnal/tailwind.config.js
+++ frontend/daumnal/tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {
+  extend: {
+    colors: {
+      bg_main: '#EBE3D5',
+      bg_modal: 'F8F6EE',
+      bg_button: 'FFF1DD',
+      bg_nav: 'F3EEEA',
+      font_main: '#696864',
+      emo_happy: 'FEFFB6',
+      emo_sad: '72AFFF',
+      emo_angry: 'FF7B76',
+      emo_scary: 'FF7B76',
+      emo_hate: '00AF7E',
+      emo_surprise: 'CC9CFF',
+      emo_middle: 'ADADAD',
+    }
   },
-  plugins: [],
-}
+};
+export const plugins = [];
 
