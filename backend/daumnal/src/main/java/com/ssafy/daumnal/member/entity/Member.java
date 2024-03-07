40d73816eab97f4d037bdd79c05b5162296b26d4
김예지 <ilice0403@naver.com>
2024-03-07 15:47:56 +0900

    feat: Member, MemberStatus, SocialProvider 생성
    - Member 엔티티
      - id 외 다른 컬럼 추가
    - MemberStatus Enum 생성
    - SocialProvider Enum 생성
    - Diary 컬럼 일부 추가

--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/entity/Member.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/entity/Member.java
@@ -3,6 +3,7 @@ package com.ssafy.daumnal.member.entity;
 import com.ssafy.daumnal.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Getter
@@ -13,4 +14,19 @@ public class Member extends BaseEntity {
     @Column(name = "id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "social_id", nullable = false)
+    private Long socialId;
+
+    @Column(name = "social_provider", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private String socialProvider;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.ORDINAL)
+    @ColumnDefault("1")
+    private int status;
 }
