17168cd75473c7fd2d1b37edef65d30ac8f285e1
김예지 <ilice0403@naver.com>
2024-03-07 15:29:25 +0900

    feat: S3Service, S3ServiceImpl 생성

--- backend/daumnal/src/main/java/com/ssafy/daumnal/s3/service/impl/S3ServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/s3/service/impl/S3ServiceImpl.java
@@ -1,4 +1,21 @@
 package com.ssafy.daumnal.s3.service.impl;
 
-public class S3ServiceImpl {
+import com.ssafy.daumnal.diary.entity.Diary;
+import com.ssafy.daumnal.music.entity.Playlist;
+import com.ssafy.daumnal.s3.service.S3Service;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class S3ServiceImpl implements S3Service {
+
+    @Override
+    public String uploadDiaryPhoto(MultipartFile diaryPhotoFile, Diary diary) {
+        return null;
+    }
+
+    @Override
+    public String uploadPlaylistCover(MultipartFile playlistCoverFile, Playlist playlist) {
+        return null;
+    }
 }
