6ff5a24eb793d972dcc8f2bf2147736ad52aa3c7
김예지 <ilice0403@naver.com>
2024-03-07 17:38:47 +0900

    feat: S3ServiceImpl upload 사진 및 파일 등 업로드 메소드 생성

--- backend/daumnal/src/main/java/com/ssafy/daumnal/s3/service/impl/S3ServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/s3/service/impl/S3ServiceImpl.java
@@ -1,7 +1,11 @@
 package com.ssafy.daumnal.s3.service.impl;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.ssafy.daumnal.diary.entity.Diary;
+import com.ssafy.daumnal.global.constants.ErrorCode;
 import com.ssafy.daumnal.music.entity.Playlist;
 import com.ssafy.daumnal.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +25,31 @@ public class S3ServiceImpl implements S3Service {
     private String bucket;
     @Value("${cloud.aws.region.static}")
     private String region;
+
+    /**
+     * s3에 파일 (이미지 등) 업로드
+     *
+     * @param file 업로드할 파일
+     * @param path 파일을 업로드할 경로
+     * @return 업로드한 파일 url
+     * @throws IOException
+     */
+    public String upload(MultipartFile file, String path) throws IOException {
+        String fileName = getFileUuidName(
+                Objects.requireNonNull(file.getOriginalFilename(), ErrorCode.NOT_EXISTS_FILE.getMessage())
+        );
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(file.getContentType());
+        metadata.setContentLength(file.getSize());
+        //S3에 파일 업로드
+        amazonS3Client.putObject(
+                //외부에 공개하는 파일인 경우 Public Read 권한을 추가
+                new PutObjectRequest(bucket, path + "/" + fileName, file.getInputStream(), metadata)
+                        .withCannedAcl(CannedAccessControlList.PublicRead)
+        );
+
+        return amazonS3Client.getUrl(bucket, path + "/" + fileName).toString();
+    }
 
     /**
      * s3에 일기 내용 중 사진 업로드
