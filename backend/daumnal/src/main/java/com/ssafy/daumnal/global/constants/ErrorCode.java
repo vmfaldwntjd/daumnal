9ccc84b82d3b1d999fa6f83020fbf610c960e86d
core <jinjs6@gmail.com>
2024-03-11 15:44:00 +0900

    fix: ErrorCode 내용 수정
    - 피드백 반영에 따라 에러 내용 중 pk 문구를 id 문구로 수정하였습니다.
    - 닉네임 등록 관련 예외 처리는 Exists 에러로 처리하는 것이 의미적으로
      맞는 것 같아 수정하였습니다.

--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/ErrorCode.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/ErrorCode.java
@@ -15,6 +15,7 @@ public enum ErrorCode {
 
     //회원
     EXISTS_MEMBER(BAD_REQUEST, "존재한 회원입니다!"),
+    EXISTS_MEMBER_NICKNAME_STATUS(BAD_REQUEST, "이미 닉네임이 등록된 회원입니다!"),
 
     NOT_EXISTS_MEMBER_ID(BAD_REQUEST, "존재하지 않는 회원 id 입니다."),
     NOT_EXISTS_MEMBER(NOT_FOUND, "존재하지 않는 회원입니다!"),
@@ -23,14 +24,13 @@ public enum ErrorCode {
     NOT_EXISTS_MEMBER_NICKNAME_INPUT(BAD_REQUEST, "닉네임을 입력해주세요!"),
     NOT_EXISTS_MEMBER_NICKNAME_GET(BAD_REQUEST, "닉네임 정보가 없습니다!"),
 
-    INVALID_MEMBER_ID(BAD_REQUEST, "회원 pk 입력 형식이 올바르지 않습니다!"),
+    INVALID_MEMBER_ID(BAD_REQUEST, "회원 id 입력 형식이 올바르지 않습니다!"),
     INVALID_MEMBER_TOKEN(FORBIDDEN, "유효하지 않는 토큰입니다!"),
     INVALID_MEMBER_SOCIAL_ID(BAD_REQUEST, "socialId의 입력 형식이 올바르지 않습니다!"),
     INVALID_MEMBER_SOCIAL_PROVIDER(BAD_REQUEST, "올바른 socialProvider를 입력해주세요!"),
     INVALID_MEMBER_NICKNAME_WORDS(BAD_REQUEST, "닉네임 입력시 한글 또는 영어로 입력해주세요!"),
     INVALID_MEMBER_NICKNAME_LENGTH(BAD_REQUEST, "닉네임 입력 길이는 15자 이하로 입력 바랍니다!"),
     INVALID_MEMBER_NICKNAME_SPACE(BAD_REQUEST, "닉네임 입력시 공백 없이 입력해주세요!"),
-    INVALID_MEMBER_NICKNAME_STATUS(BAD_REQUEST, "이미 닉네임이 등록된 회원입니다!"),
     INVALID_MEMBER_STATUS_ON_LOGOUT(FORBIDDEN, "해당 회원은 로그아웃 한 상태입니다!"),
     INVALID_MEMBER_STATUS_ON_DELETE(FORBIDDEN, "해당 회원은 탈퇴 처리된 회원입니다!"),
 
@@ -43,8 +43,8 @@ public enum ErrorCode {
     NOT_EXISTS_DIARY_YEAR_INPUT(BAD_REQUEST, "year 정보를 요청받지 못하였습니다!"),
     NOT_EXISTS_DIARY_MONTH_INPUT(BAD_REQUEST, "month 정보를 요청받지 못하였습니다!"),
 
-    INVALID_DIARY_ID(BAD_REQUEST, "일기 pk 입력 형식이 올바르지 않습니다!"),
-    INVALID_DIARY_EMOTION_ID(BAD_REQUEST, "감정 pk 입력 형식이 올바르지 않습니다!"),
+    INVALID_DIARY_ID(BAD_REQUEST, "일기 id 입력 형식이 올바르지 않습니다!"),
+    INVALID_DIARY_EMOTION_ID(BAD_REQUEST, "감정 id 입력 형식이 올바르지 않습니다!"),
     INVALID_DIARY_CONTENT_LENGTH(BAD_REQUEST, "일기 내용은 3000자 이하로 작성해주세요!"),
     INVALID_DIARY_HASHTAG_LENGTH(BAD_REQUEST, "해시태그의 글자수는 10자 이하여야 합니다!"),
     INVALID_DIARY_HASHTAG_WORDS(BAD_REQUEST, "해시태그의 입력 방법이 올바르지 않습니다!"),
@@ -55,7 +55,7 @@ public enum ErrorCode {
     //음악
     NOT_EXISTS_BACKGROUND_MUSIC(NOT_FOUND, "존재하지 않는 배경음악입니다!"),
 
-    INVALID_BACKGROUND_MUSIC_ID(BAD_REQUEST, "배경음악 pk 입력 형식이 올바르지 않습니다!"),
+    INVALID_BACKGROUND_MUSIC_ID(BAD_REQUEST, "배경음악 id 입력 형식이 올바르지 않습니다!"),
 
     //파일
     NOT_EXISTS_FILE(BAD_REQUEST, "존재하지 않는 파일입니다.");
