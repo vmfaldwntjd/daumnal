556a0f85486c6025b22db603938557038f3da9b5
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-03-11 14:01:46 +0900

    feat: 캐릭터 선택 페이지 기본 설정

--- frontend/daumnal/src/pages/SelectCharacterPage.tsx
+++ frontend/daumnal/src/pages/SelectCharacterPage.tsx
@@ -1,1 +1,25 @@
-export {};
+import React from 'react';
+import styled from 'styled-components';
+import CharacterCard from '../components/music/CharacterCard';
+
+const Characters = styled.div`
+  display: flex;
+  margin: 0px 30px;
+`
+
+const SelectCharacterPage: React.FC = () => {
+  return (
+    <div>
+      {/* 페이지 문구 */}
+      <p className='m-10 text-3xl'>오늘의 나무를 골라 주세요!</p>
+      <Characters>
+        <CharacterCard imageUrl="/image/dabom.png" />
+        <CharacterCard imageUrl="/image/dareum.png" />
+        <CharacterCard imageUrl="/image/daeul.png" />
+        <CharacterCard imageUrl="/image/dagyeol.png" />
+      </Characters>
+    </div>
+  );
+};
+
+export default SelectCharacterPage;
