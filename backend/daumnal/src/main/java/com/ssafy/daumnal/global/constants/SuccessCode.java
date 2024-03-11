8afb1952d1bc39a5a771e218e9642df326e9cd5b
core <jinjs6@gmail.com>
2024-03-11 17:43:24 +0900

    feat: 성공 코드 문구 정리
    api 명세서에 작성된 성공 코드 내용을 바탕으로 코드에 반영하였습니다.
    crud 관련한 api 기능에 대한 성공 코드는 각각 CREATE, GET, UPDATE,
    DELETE의 접두어를 붙였고, 리스트 형태의 반환과 관련된 성공 코드는
    GET_BACKGROUND_MUSICS과 같이 뒤에 S를 붙여서 리스트 표시를 해주었습니다.
    리스트가 아닌 단일 데이터 조회 성공은 S를 제거해줌으로써 성공 코드
    내용을 설정해주었습니다.

--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/SuccessCode.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/constants/SuccessCode.java
@@ -14,9 +14,30 @@ public enum SuccessCode {
     //일반
     UPLOAD_IMAGE(CREATED, "이미지 업로드 성공"),
 
+    //회원
+    CREATE_MEMBER(CREATED, "회원 정보 등록에 성공하였습니다!"),
+    CREATE_MEMBER_NICKNAME(CREATED, "닉네임 정보 입력에 성공하였습니다!"),
+    GET_MEMBER(OK, "회원 정보 조회에 성공하였습니다!"),
+    GET_MEMBER_NICKNAME(OK, "닉네임 조회에 성공하였습니다!"),
+    UPDATE_MEMBER_NICKNAME(OK, "닉네임 정보 변경에 성공하였습니다!"),
+    UPDATE_MEMBER_LOGOUT(OK, "로그아웃에 성공하였습니다!"),
+    UPDATE_MEMBER_STATUS_DELETE(OK, "회원 탈퇴 처리를 완료하였습니다!"),
+
     //일기
     UPLOAD_DIARY_PHOTO(CREATED, "일기 내용 중 사진 업로드 성공"),
-    UPLOAD_PLAYLIST_COVER(CREATED, "플레이리스트 커버 업로드 성공");
+    UPLOAD_PLAYLIST_COVER(CREATED, "플레이리스트 커버 업로드 성공"),
+    CREATE_DIARY(OK, "선택한 일기 정보 조회에 성공하였습니다!"),
+    GET_DIARY(CREATED, "일기 입력에 성공하였습니다!"),
+    GET_DIARY_STATUS(OK, "일기 작성 여부 결과를 성공적으로 가져왔습니다!"),
+    GET_DIARY_MONTH_EMOTION(OK, "월별 일기 감정 정보 조회에 성공하였습니다!"),
+    GET_DIARY_DAY_EMOTION(OK, "일기의 감정 정보 조회에 성공하였습니다!"),
+    GET_DIARY_CALENDAR(OK, "캘린더 정보 조회에 성공하였습니다!"),
+    DELETE_DIARY(OK, "일기 삭제를 완료하였습니다!"),
+
+    //음악
+    GET_BACKGROUND_MUSICS(OK, "배경 음악 정보 조회에 성공하였습니다!"),
+    GET_BACKGROUND_MUSIC(OK, "선택한 배경 음악 정보 조회에 성공하였습니다!"),
+    UPDATE_BACKGROUND_MUSIC(OK, "배경 음악 정보를 변경 완료하였습니다!");
 
     private final HttpStatus status;
     private final String message;
