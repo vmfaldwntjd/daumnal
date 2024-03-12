43a016e03b3c16a92aab443069fe8e81ac1e03d5
core <jinjs6@gmail.com>
2024-03-12 12:36:44 +0900

    feat: 회원 닉네임 입력 관련 예외처리 진행
    예외처리 사항은 다음과 같습니다.
    - memberId 자연수 입력 여부
    - 로그아웃 여부
    - 회원 탈퇴 여부
    - 회원 존재 여부
    - 닉네임 입력 여부
    - 닉네임 한글 또는 영어 입력 여부
    - 닉네임 길이 15자 이하 제한
    - 이미 존재한 닉네임을 입력 제한

--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/service/impl/MemberServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/service/impl/MemberServiceImpl.java
@@ -13,8 +13,6 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 import static com.ssafy.daumnal.global.constants.ErrorCode.*;
 
 @Service
@@ -24,7 +22,13 @@ public class MemberServiceImpl implements MemberService {
     private static final String KAKAO = "kakao";
     private static final String NAVER = "naver";
     private static final String NUMBER_REGEX = "^[1-9]\\d*$";
+    private static final String KOREAN_REGEX = "^[가-힣]+$";
+    private static final String ENGLISH_REGEX = "^[a-zA-Z]+$";
 
+    private static final int MEMBER_DELETE = 0;
+    private static final int MEMBER_LOGOUT = 2;
+
+    private static final int NICKNAME_MAX_LENGTH = 15;
 
     private final MemberRepository memberRepository;
 
@@ -70,11 +74,49 @@ public class MemberServiceImpl implements MemberService {
     @Transactional
     @Override
     public void addMemberNickname(String memberId, AddMemberNicknameRequest nicknameRequest) {
-        // 회원 pk 찾아오기
+        // 회원 pk 입력이 올바른지 확인하기
+        if (!memberId.matches(NUMBER_REGEX)) {
+            throw new InvalidException(INVALID_MEMBER_ID);
+        }
+
+        // 회원 pk 찾아오기 -> 존재하지 않으면 예외처리
         Member member = memberRepository.findById(Long.parseLong(memberId))
                 .orElseThrow(() -> new NoExistException(NOT_EXISTS_MEMBER_ID));
 
+        int memberStatus = member.getStatus().getValue();
+
+        // 회원 상태 확인하기 -> 탈퇴된 회원인 경우
+        if (memberStatus == MEMBER_DELETE) {
+            throw new InvalidException(INVALID_MEMBER_STATUS_ON_DELETE);
+        }
+
+        // 로그아웃 한 회원인 경우
+        if (memberStatus == MEMBER_LOGOUT) {
+            throw new InvalidException(INVALID_MEMBER_STATUS_ON_LOGOUT);
+        }
+
         String nickname = nicknameRequest.getMemberNickname();
+
+        // 닉네임을 입력 안 한 경우
+        if (nickname == null) {
+            throw new NoExistException(NOT_EXISTS_MEMBER_NICKNAME_INPUT);
+        }
+
+        // 닉네임 입력이 한글 또는 영어가 아닌 경우
+        if (!(nickname.matches(ENGLISH_REGEX) || nickname.matches(KOREAN_REGEX))) {
+            throw new InvalidException(INVALID_MEMBER_NICKNAME_WORDS);
+        }
+
+        // 닉네임 입력 길이가 15자가 넘어가버린 경우
+        if (nickname.length() > NICKNAME_MAX_LENGTH) {
+            throw new InvalidException(INVALID_MEMBER_NICKNAME_LENGTH);
+        }
+
+        // 이미 존재한 닉네임을 입력하는 경우
+        if (member.getNickname() != null && memberRepository.existsMemberByNickname(nickname)) {
+            throw new ExistException(EXISTS_MEMBER_NICKNAME_STATUS);
+        }
+
         member.updateNickname(nickname);
     }
 
