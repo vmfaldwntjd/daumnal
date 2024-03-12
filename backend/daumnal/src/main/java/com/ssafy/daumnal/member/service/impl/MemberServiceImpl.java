634a4096f577b0264cbc2d4d83b0561b0ed77f42
core <jinjs6@gmail.com>
2024-03-12 09:45:45 +0900

    feat: 회원가입 기능 구현(예외처리는 아직 안 한 상태입니다.)

--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/service/impl/MemberServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/service/impl/MemberServiceImpl.java
@@ -1,7 +1,11 @@
 package com.ssafy.daumnal.member.service.impl;
 
+import com.ssafy.daumnal.member.dto.MemberDTO.AddMemberRequest;
+import com.ssafy.daumnal.member.entity.Member;
+import com.ssafy.daumnal.member.entity.SocialProvider;
 import com.ssafy.daumnal.member.repository.MemberRepository;
 import com.ssafy.daumnal.member.service.MemberService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +13,36 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
 
+    private static final String KAKAO = "kakao";
+    private static final String NAVER = "naver";
+
+
     private final MemberRepository memberRepository;
+
+    @Transactional
+    @Override
+    public void addMember(AddMemberRequest addMemberRequest) {
+
+        SocialProvider socialProvider = getProvider(addMemberRequest.getSocialProvider());
+
+        Member member = Member.builder()
+                .socialId(Long.parseLong(addMemberRequest.getSocialId()))
+                .socialProvider(socialProvider)
+                .build();
+
+        memberRepository.save(member);
+    }
+
+    private SocialProvider getProvider(String socialProvider) {
+
+        SocialProvider providerResult = null;
+
+        if (socialProvider.equals(KAKAO)) {
+            providerResult = SocialProvider.KAKAO;
+        } else if (socialProvider.equals(NAVER)) {
+            providerResult = SocialProvider.NAVER;
+        }
+
+        return providerResult;
+    }
 }
