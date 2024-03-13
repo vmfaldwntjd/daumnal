f5582ff979f82efc7c58ae01db47f4dc2f8b8ec1
core <jinjs6@gmail.com>
2024-03-13 14:45:33 +0900

    test: Jwt 토큰 생성 테스트 코드 작성
    Former-commit-id: a51f16417b04bc7c7e16c9498d106c519c506e86

---
+++ backend/daumnal/src/test/java/com/ssafy/daumnal/global/util/JwtProviderTest.java
@@ -0,0 +1,66 @@
+package com.ssafy.daumnal.global.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+import static com.ssafy.daumnal.global.constants.JwtConstants.*;
+
+@SpringBootTest
+@Slf4j
+class JwtProviderTest {
+
+    private SecretKey key;
+
+    @BeforeEach
+    void init() {
+        String secret = "jxgEQeXHuPq8VdbyYFNkANdudQ53YUasjioejalksdjfaoigjioejfakls123diojseoifjdklfjwogjdfkjgoszzdjfeiofjoidjf4";
+        key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+    }
+
+    @Test
+    void generateToken() {
+        //given
+        Long memberId = 1L;
+        Long socialId = 12L;
+        String socialProvider = "kakao";
+        Long accessExpiresIn = 300000000L;
+
+        //when
+        String accessToken = Jwts.builder()
+                .issuer(ISSUER)
+                .subject(String.valueOf(memberId))
+                .issuedAt(new Date())
+                .expiration(new Date(accessExpiresIn))
+                .claim(ID_CATEGORY, socialId)
+                .claim(PROVIDER_CATEGORY, socialProvider)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+
+        String accessTokenAnother = Jwts.builder()
+                .issuer(ISSUER)
+                .subject(String.valueOf(memberId))
+                .issuedAt(new Date())
+                .expiration(new Date(accessExpiresIn))
+                .claim(ID_CATEGORY, socialId)
+                .claim(PROVIDER_CATEGORY, socialProvider)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+
+        log.info("accessToken={}", accessToken);
+        log.info("accessTokenAnother={}", accessTokenAnother);
+
+        //then
+        Assertions.assertThat(accessToken)
+                .isEqualTo(accessTokenAnother);
+    }
+}
