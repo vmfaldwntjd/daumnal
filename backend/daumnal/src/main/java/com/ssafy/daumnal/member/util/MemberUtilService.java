c900f325efbf39d921424b5ed853f5e884e9ce1b
core <jinjs6@gmail.com>
2024-03-14 14:08:27 +0900

    refactor: addMemberNickname 예외처리 관련 코드 리팩토링 진행

--- backend/daumnal/src/main/java/com/ssafy/daumnal/member/util/MemberUtilService.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/member/util/MemberUtilService.java
@@ -52,5 +52,46 @@ public class MemberUtilService {
             throw new InvalidException(INVALID_MEMBER_SOCIAL_PROVIDER);
         }
     }
-    
+
+    /**
+     * memberId의 값이 자연수인지 검증하기
+     * @param memberId
+     */
+    public void validateMemberIdNumber(String memberId) {
+        if (!memberId.matches(NUMBER_REGEX)) {
+            throw new InvalidException(INVALID_MEMBER_ID);
+        }
+    }
+
+    /**
+     * 회원 상태가 로그인 상태인지 검증하기
+     * @param memberStatus
+     */
+    public void validateMemberStatusLogin(int memberStatus) {
+        if (memberStatus == MEMBER_DELETE) {
+            throw new InvalidException(INVALID_MEMBER_STATUS_ON_DELETE);
+        }
+
+        if (memberStatus == MEMBER_LOGOUT) {
+            throw new InvalidException(INVALID_MEMBER_STATUS_ON_LOGOUT);
+        }
+    }
+
+    /**
+     * 닉네임 입력 검증하기
+     * @param nickname
+     */
+    public void validateInputMemberNickname(String nickname) {
+        if (nickname == null) {
+            throw new NoExistException(NOT_EXISTS_MEMBER_NICKNAME_INPUT);
+        }
+
+        if (!(nickname.matches(ENGLISH_REGEX) || nickname.matches(KOREAN_REGEX))) {
+            throw new InvalidException(INVALID_MEMBER_NICKNAME_WORDS);
+        }
+
+        if (nickname.length() > NICKNAME_MAX_LENGTH) {
+            throw new InvalidException(INVALID_MEMBER_NICKNAME_LENGTH);
+        }
+    }
 }
