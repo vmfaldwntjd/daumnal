f5f95af38264e6349bda0aa7e44174d53a9ab6f5
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-03-14 14:12:29 +0900

    feat: 로딩 페이지 기본 설정

--- frontend/daumnal/src/pages/LoadingPage.tsx
+++ frontend/daumnal/src/pages/LoadingPage.tsx
@@ -1,17 +1,55 @@
 // 로딩 페이지
 import React from 'react';
 import styled from 'styled-components';
-import CharacterCard from '../components/music/CharacterCard';
 
-const Characters = styled.div`
+const Images = styled.div`
+  width: 100%;
+  height: 180px;
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
+  gap: 8px;
+`;
+
+const Image = styled.img`
+  width: 150px;
+`;
+
+const Text = styled.p`
+  width: 1200px;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 30px;
+  border-radius: 40px;
+  font-size: 60px;
+  background-color: #F8F6EE;
 `;
 
 const LoadingPage: React.FC = () => {
   return (
-    <div className="p-9">
-      <p className="text-4xl mb-7">- - - - - 일기 내용 감정 분석 중 - - - - -</p>
+    <div className="flex flex-col items-center p-10">
+      <Images>
+        <Image src="/image/dabom.png" />
+        <Image src="/image/dareum.png" />
+        <Image src="/image/daeul.png" />
+        <Image src="/image/dagyeol.png" />
+        <Image src="/image/dabom.png" />
+        <Image src="/image/dareum.png" />
+        <Image src="/image/daeul.png" />
+        <Image src="/image/dagyeol.png" />
+      </Images>
+      <Text>오늘 적은 일기에서 감정을 추출하고 있어요!</Text>
+      <Images>
+        <Image src="/image/dagyeol.png" />
+        <Image src="/image/daeul.png" />
+        <Image src="/image/dareum.png" />
+        <Image src="/image/dabom.png" />
+        <Image src="/image/dagyeol.png" />
+        <Image src="/image/daeul.png" />
+        <Image src="/image/dareum.png" />
+        <Image src="/image/dabom.png" />
+      </Images>
     </div>
   );
 };
