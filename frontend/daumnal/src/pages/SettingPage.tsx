f6bf6709d10a68f29cf6a38be5bd7f58cde417e7
정성현 <gkgktjdgusdl@naver.com>
2024-03-15 14:09:20 +0900

    feat : 설정 페이지 구현

--- frontend/daumnal/src/pages/SettingPage.tsx
+++ frontend/daumnal/src/pages/SettingPage.tsx
@@ -23,12 +23,49 @@ const SettingPage: React.FC = () => {
   };
 
   return (
-    <div className='text-center mt-20'>
-      <h1>Setting</h1>
-      <button className='mt-5 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700' onClick={logoutWithKakao}>
-        로그아웃
-      </button>
+    <div className='text-center mt-20' style={{ 
+      display: 'flex', 
+      flexDirection: 'column', 
+      alignItems: 'center', 
+      justifyContent: 'center', 
+      height: '85vh', // 부모 컨테이너의 높이를 전체 화면으로 설정
+    }}>
+      <div style={{ 
+        fontSize: '35px',
+        marginTop: '10px',
+      }}>
+        익명의 일기장
+      </div>
+      <hr style={{
+        width: '823px',
+        backgroundColor: '#696864',
+        height: '1.5px',
+        border: 'none',
+        marginTop: '10px',
+      }}/>
+      <div className="image-container" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
+        <img
+          src="./image/setting_nickname.png"
+          alt="닉네임 수정"
+          className="mt-10 hover:cursor-pointer"
+          style={{ width: '823px', height: '82px' }}
+        />
+        <img
+          src="./image/setting_BGM.png"
+          alt="BGM 변경"
+          className="mt-10 hover:cursor-pointer"
+          style={{ width: '823px', height: '82px' }}
+        />
+        <img
+          src="./image/setting_logout.png"
+          alt="로그아웃"
+          className="mt-10 hover:cursor-pointer"
+          style={{ width: '823px', height: '82px' }}
+          onClick={logoutWithKakao}
+        />
+      </div>
     </div>
+    
   );
 };
 
