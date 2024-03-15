90efb0c56ec0a87b2cd4b0ced8296da7ed1901c7
정성현 <gkgktjdgusdl@naver.com>
2024-03-15 17:28:27 +0900

    feat : 월별 감정 분석 네모 칸 제작

--- frontend/daumnal/src/pages/CalendarPage.tsx
+++ frontend/daumnal/src/pages/CalendarPage.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const CalendarPage: React.FC = () => {
   const navigate = useNavigate(); 
 
+  const today: Date = new Date()
+  const thisMonth:Number = today.getMonth() + 1
+  const thisYear:Number = today.getFullYear();
+
+  const [year, setYear] = useState<Number>(thisYear)
+  const [month, setMonth] = useState<Number>(thisMonth)
+
   const handleButtonClick = () => {
-    navigate('/monthly-result');
+    navigate('/monthlyresultpage', { state: { year, month } });
   };
 
   return (
