b8d9f5657d4877ecf3b7fb481aae8ba74f4b2a74
core <jinjs6@gmail.com>
2024-03-18 16:42:28 +0900

    chore: 필요 없는 코드 제거

--- backend/daumnal/src/main/java/com/ssafy/daumnal/global/config/SecurityConfig.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/global/config/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.Collections;
