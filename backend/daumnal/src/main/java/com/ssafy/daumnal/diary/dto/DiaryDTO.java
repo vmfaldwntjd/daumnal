44faa16799e6bf773343eac55a5737cf57d4f8bc
core <jinjs6@gmail.com>
2024-03-19 15:49:51 +0900

    feat: 일기 입력 request dto 생성
    Former-commit-id: 308cb484b945a623b42d2142e0c6f862558dbb0d

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/dto/DiaryDTO.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/dto/DiaryDTO.java
@@ -1,8 +1,29 @@
 package com.ssafy.daumnal.diary.dto;
 
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 public class DiaryDTO {
+
+    @Getter
+    public static class DiaryRequest {
+        private String diaryTitle;
+        private String diaryContent;
+        private String diaryHashTag;
+        private MultipartFile diaryPhoto;
+        private DiaryEmotion diaryEmotion;
+    }
+
+    @AllArgsConstructor
+    private static class DiaryEmotion {
+        private Integer fear;
+        private Integer surprise;
+        private Integer angry;
+        private Integer sadness;
+        private Integer neutral;
+        private Integer happiness;
+        private Integer disgust;
+    }
 
     @Getter
     @Setter
