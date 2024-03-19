f017e07fe10c08a5e29b7743636c0552344a5802
jojoo <csh990720@gmail.com>
2024-03-19 21:46:55 +0900

    fix: redirect url 재수정

--- frontend/daumnal/src/pages/LoginPage.tsx
+++ frontend/daumnal/src/pages/LoginPage.tsx
@@ -7,7 +7,7 @@ const LoginPage: React.FC = () => {
   const loginWithKakao = async () => {
     try {
       await Kakao.Auth.authorize({
-        redirectUri: `${process.env.REACT_APP_LOCAL_BASE_URL}/oauth`,
+        redirectUri: `${process.env.REACT_APP_SPRINGBOOT_BASE_URL}/oauth`,
         scope: 'profile_nickname',
         prompt: 'select_account',
       });
