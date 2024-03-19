88f610945dc96255336e89fd2a7cc8be613a3970
core <jinjs6@gmail.com>
2024-03-19 14:09:09 +0900

    feat: 일기 작성 여부 api 구현
    Former-commit-id: 9837ff78d051c203d689747e56c59428944aca49

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/impl/DiaryServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/impl/DiaryServiceImpl.java
@@ -1,13 +1,43 @@
 package com.ssafy.daumnal.diary.service.impl;
 
+import com.ssafy.daumnal.diary.dto.DiaryDTO.GetDiaryWrittenTodayResponse;
 import com.ssafy.daumnal.diary.repository.DiaryRepository;
 import com.ssafy.daumnal.diary.service.DiaryService;
+import com.ssafy.daumnal.global.exception.NoExistException;
+import com.ssafy.daumnal.member.entity.Member;
+import com.ssafy.daumnal.member.repository.MemberRepository;
+import com.ssafy.daumnal.member.util.MemberUtilService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.ssafy.daumnal.global.constants.ErrorCode.NOT_EXISTS_MEMBER_ID;
 
 @Service
 @RequiredArgsConstructor
 public class DiaryServiceImpl implements DiaryService {
 
     private final DiaryRepository diaryRepository;
+    private final MemberUtilService memberUtilService;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public GetDiaryWrittenTodayResponse getDiaryWritten(String memberId) {
+
+        GetDiaryWrittenTodayResponse writtenResponse = GetDiaryWrittenTodayResponse.builder()
+                .written(false)
+                .build();
+
+        memberUtilService.validateMemberIdNumber(memberId);
+        Member member = memberRepository.findById(Long.parseLong(memberId))
+                .orElseThrow(() -> new NoExistException(NOT_EXISTS_MEMBER_ID));
+
+        memberUtilService.validateMemberStatusNotDelete(member.getStatus().getValue());
+        memberUtilService.validateMemberStatusNotLogout(member.getStatus().getValue());
+
+        if (diaryRepository.existsByMember(member)) {
+            writtenResponse.setWritten(true);
+        }
+
+        return writtenResponse;
+    }
 }
