7523d48878800e5aeb79d688189344fb33c3e2d4
jojoo <csh990720@gmail.com>
2024-03-20 18:02:36 +0900

    feat: 캘린더 변수 전달/ 모달 연결

--- frontend/daumnal/src/components/modal/DiaryDetailModal.tsx
+++ frontend/daumnal/src/components/modal/DiaryDetailModal.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 interface DiaryDetailModalProps {
     onClose: () => void; // onClose는 함수이며, 반환 값이 없음을 의미합니다.
@@ -9,11 +11,18 @@ const DiaryDetailModal: React.FC<DiaryDetailModalProps> = ({ onClose }) => {
 
 
     return (
-        <div className="fixed mr-[134px] inset-0 bg-black bg-opacity-30 flex justify-center items-center" onClick={onClose}>
-          <div className="bg-bg_modal p-8 rounded-xl h-[90%] w-[600px]" onClick={(e) => e.stopPropagation()}>
+        <div className="fixed mr-[134px] inset-0 bg-black bg-opacity-40 flex justify-center items-center" onClick={onClose}>
+          <button className=' m-auto'>
+            <FontAwesomeIcon icon={faChevronLeft} size="2xl" className="text-[60px]" style={{color: "#cccccc"}} />
+          </button>    
+          <div className="bg-bg_modal p-8 rounded-xl h-[90%] w-[600px] shadow-lg" onClick={(e) => e.stopPropagation()}>
             {/* 여기에 모달 내용을 추가하세요 */}
             <p>여기는 모달 내용입니다.</p>
           </div>
+          <button className=' m-auto'>
+            <FontAwesomeIcon icon={faChevronRight} size="2xl" className="text-[60px]" style={{color: "#cccccc"}} />            
+          </button>
+
         </div>
       );
 }
