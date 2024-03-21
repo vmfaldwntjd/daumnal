71aaf08131f6f4ad59d4018a84a37000f39c8ba2
jojoo <csh990720@gmail.com>
2024-03-21 17:28:05 +0900

    feat: 일기 등록 통신 성공/ 캘린더 페이지 수정

--- frontend/daumnal/src/components/modal/DiaryDetailModal.tsx
+++ frontend/daumnal/src/components/modal/DiaryDetailModal.tsx
@@ -1,29 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import DailyResultModal from './DailyResultModal';
 
 interface DiaryDetailModalProps {
-    onClose: () => void; // onClose는 함수이며, 반환 값이 없음을 의미합니다.
+  onDiaryModalClose: () => void; 
   }
 
-const DiaryDetailModal: React.FC<DiaryDetailModalProps> = ({ onClose }) => {
+const DiaryDetailModal: React.FC<DiaryDetailModalProps> = ({ onDiaryModalClose }) => {
+
+  const [isDailyResultModalOpen, setIsDailyResultModalOpen] = useState<boolean>(false)
 
 
 
     return (
-        <div className="fixed mr-[134px] inset-0 bg-black bg-opacity-40 flex justify-center items-center" onClick={onClose}>
+      <div>
+        { !isDailyResultModalOpen &&
+        <div className="fixed mr-[134px] inset-0 bg-black bg-opacity-40 flex justify-center items-center" onClick={onDiaryModalClose}>
           <button className=' m-auto'>
             <FontAwesomeIcon icon={faChevronLeft} size="2xl" className="text-[60px]" style={{color: "#cccccc"}} />
           </button>    
           <div className="bg-bg_modal p-8 rounded-xl h-[90%] w-[600px] shadow-lg" onClick={(e) => e.stopPropagation()}>
             {/* 여기에 모달 내용을 추가하세요 */}
+            <button onClick={() => setIsDailyResultModalOpen(true)} className='border text-sm py-2 px-2 border-button_border bg-bg_button rounded-lg lg:text-[17px] lg:px-4'>오늘의 감정분석 보러가기</button>
             <p>여기는 모달 내용입니다.</p>
           </div>
           <button className=' m-auto'>
             <FontAwesomeIcon icon={faChevronRight} size="2xl" className="text-[60px]" style={{color: "#cccccc"}} />            
           </button>
+        </div>}
+        { isDailyResultModalOpen && <DailyResultModal onDailyResultModalClose={()=> setIsDailyResultModalOpen(false)}/>}
+      </div>
 
-        </div>
       );
 }
 
