396a472c06a462150bbfaa57c29566aca3a6f908
정성현 <gkgktjdgusdl@naver.com>
2024-03-22 17:30:31 +0900

    feat : 일별 감정 조회 디자인 초기 작업

--- frontend/daumnal/src/components/EmotionButton.tsx
+++ frontend/daumnal/src/components/EmotionButton.tsx
@@ -13,7 +13,7 @@ const EmotionButton: React.FC<EmotionButtonProps> = ({ emotion, isSelected, togg
   // 버튼 크기를 컨테이너 크기에 맞게 계산
   const buttonStyle = {
     width: isSelected ? `${containerSize.width * 0.07474}px` : `${containerSize.width * 0.03518}px`,
-    height: `${containerSize.height * 0.9718}px`
+    // height: `${containerSize.height * 0.9718}px`
   };
 
   return (
