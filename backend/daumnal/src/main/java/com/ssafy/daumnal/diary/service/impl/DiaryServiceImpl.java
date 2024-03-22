822e9bec7613476d002d917d6948ce73f03857e4
core <jinjs6@gmail.com>
2024-03-22 15:41:39 +0900

    refactor: 클라이언트와의 합의에 따른 타입 변경
    Former-commit-id: dd177c13019577737773fa135d203fcce5126670

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/impl/DiaryServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/service/impl/DiaryServiceImpl.java
@@ -102,13 +102,13 @@ public class DiaryServiceImpl implements DiaryService {
         }
 
         Emotion emotion = Emotion.builder()
-                .fear(Integer.parseInt(diaryEmotion.getFear()))
-                .surprise(Integer.parseInt(diaryEmotion.getSurprise()))
-                .angry(Integer.parseInt(diaryEmotion.getAngry()))
-                .sadness(Integer.parseInt(diaryEmotion.getSadness()))
-                .neutral(Integer.parseInt(diaryEmotion.getNeutral()))
-                .happiness(Integer.parseInt(diaryEmotion.getHappiness()))
-                .disgust(Integer.parseInt(diaryEmotion.getDisgust()))
+                .fear(diaryEmotion.getFear())
+                .surprise(diaryEmotion.getSurprise())
+                .angry(diaryEmotion.getAngry())
+                .sadness(diaryEmotion.getSadness())
+                .neutral(diaryEmotion.getNeutral())
+                .happiness(diaryEmotion.getHappiness())
+                .disgust(diaryEmotion.getDisgust())
                 .build();
         emotionRepository.save(emotion);
 
