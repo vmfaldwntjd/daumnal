30cf6ffdcb30fa9eb542dfe772b724956cea54ae
김예지 <ilice0403@naver.com>
2024-03-22 16:33:55 +0900

    feat: 플레이리스트에 노래 추가 API 기능 구현
    - PlaylistController
      - addMusicToPlaylist 메소드 생성
    - PlaylistService
      - addMusicToPlaylist 메소드 생성
    - PlaylistServiceImpl
      - addMusictoPlaylist 메소드 생성
        - 회원 검증 기능
        - 플레이리스트 검증 기능
          - 30개 이상 저장 불가능
        - 노래 검증 기능
        - 플레이리스트와 노래 테이블에 저장
    - SuccessCode
      - ADD_MUSIC_TO_PLAYLIST 성공 메시지 생성
    - ErrorCode
      - NOT_EXISTS_PLAYLIST_ID, NOT_SAME_LOGIN_MEMBER_AND_PLAYLIST_OWNER,
        PLAYLIST_AND_MUSIC_LIMIT_EXCEEDED 에러 메시지 생성
    - LimitExceededException 예외 클래스 생성
      - 제한 사항을 넘겼을 때 발생하는 예외 클래스
    - PlaylistMusicRepository
      - countByPlaylist 메소드 생성
        - 플레이리스트에 노래가 몇 개 저장됐는지 알기 위한 메소드 (30개
          저장됐는지 여부 판단)
    - PlaylistMusic
      - @Builder 있는 생성자 생성
    - GlobalExceptionHandler
      - handleLLimitExceededException 메소드 생성
        - LimitExceededException 예외 처리하는 메소드

--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/entity/PlaylistMusic.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/entity/PlaylistMusic.java
@@ -2,6 +2,7 @@ package com.ssafy.daumnal.music.entity;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
@@ -25,4 +26,10 @@ public class PlaylistMusic {
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "music_id", nullable = false)
     private Music music;
+
+    @Builder
+    public PlaylistMusic(Playlist playlist, Music music) {
+        this.playlist = playlist;
+        this.music = music;
+    }
 }
--- backend/daumnal/src/main/java/com/ssafy/daumnal/music/service/impl/PlaylistServiceImpl.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/music/service/impl/PlaylistServiceImpl.java
@@ -1,23 +1,32 @@
 package com.ssafy.daumnal.music.service.impl;
 
+import com.ssafy.daumnal.global.exception.LimitExceededException;
 import com.ssafy.daumnal.global.exception.NoExistException;
+import com.ssafy.daumnal.global.exception.NotSameException;
 import com.ssafy.daumnal.member.entity.Member;
 import com.ssafy.daumnal.member.repository.MemberRepository;
 import com.ssafy.daumnal.member.util.MemberUtilService;
 import com.ssafy.daumnal.music.dto.PlaylistDTO.AddPlaylistRequest;
+import com.ssafy.daumnal.music.entity.Music;
+import com.ssafy.daumnal.music.entity.Playlist;
+import com.ssafy.daumnal.music.entity.PlaylistMusic;
+import com.ssafy.daumnal.music.repository.MusicRepository;
+import com.ssafy.daumnal.music.repository.PlaylistMusicRepository;
 import com.ssafy.daumnal.music.repository.PlaylistRepository;
 import com.ssafy.daumnal.music.service.PlaylistService;
 import com.ssafy.daumnal.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import static com.ssafy.daumnal.global.constants.ErrorCode.NOT_EXISTS_MEMBER_ID;
+import static com.ssafy.daumnal.global.constants.ErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
 public class PlaylistServiceImpl implements PlaylistService {
 
     private final PlaylistRepository playlistRepository;
+    private final PlaylistMusicRepository playlistMusicRepository;
+    private final MusicRepository musicRepository;
     private final MemberRepository memberRepository;
     private final MemberUtilService memberUtilService;
     private final S3Service s3Service;
@@ -42,5 +51,37 @@ public class PlaylistServiceImpl implements PlaylistService {
         if (addPlaylistRequest.getPlaylistCover() == null) {
             playlistRepository.save(addPlaylistRequest.toEntityWithoutCoverUrl(member));
         }
+    }
+
+    /**
+     * 플레이리스트에 노래 추가
+     * @param memberId 로그인 상태인 회원 id
+     * @param playlistId 노래를 넣을 플레이리스트 id
+     * @param musicId 플레이리스트에 추가할 노래 id
+     */
+    @Override
+    public void addMusicToPlaylist(String memberId, Long playlistId, Long musicId) {
+        memberUtilService.validateMemberIdNumber(memberId);
+        Member member = memberRepository.findById(Long.parseLong(memberId))
+                .orElseThrow(() -> new NoExistException(NOT_EXISTS_MEMBER_ID));
+        memberUtilService.validateMemberStatusNotLogout(member.getStatus().getValue());
+        memberUtilService.validateMemberStatusNotDelete(member.getStatus().getValue());
+
+        Playlist playlist = playlistRepository.findById(playlistId)
+                        .orElseThrow(() -> new NoExistException(NOT_EXISTS_PLAYLIST_ID));
+        if (!playlist.getMember().equals(member)) {
+            throw new NotSameException(NOT_SAME_LOGIN_MEMBER_AND_PLAYLIST_OWNER);
+        }
+        if (playlistMusicRepository.countByPlaylist(playlist) >= 30) {
+            throw new LimitExceededException(PLAYLIST_AND_MUSIC_LIMIT_EXCEEDED);
+        }
+        Music music = musicRepository.findById(musicId)
+                        .orElseThrow(() -> new NoExistException(NOT_EXISTS_MUSIC_ID));
+        playlistMusicRepository.save(
+                PlaylistMusic.builder()
+                .playlist(playlist)
+                .music(music)
+                .build()
+        );
     }
 }
