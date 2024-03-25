41222364bbd9d1b5e04eba9927f8a18cb527e7c7
정성현 <gkgktjdgusdl@naver.com>
2024-03-25 17:57:13 +0900

    feat : 일별 감정 분석 거의 완료, 액세스 토큰 재발급 구현

--- frontend/daumnal/src/pages/api/axiosInstance.tsx
+++ frontend/daumnal/src/pages/api/axiosInstance.tsx
@@ -1,5 +1,4 @@
-// axiosInstance.ts
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, InternalAxiosRequestConfig, AxiosResponse } from 'axios';
 
 // Axios 인스턴스 생성
 const axiosInstance: AxiosInstance = axios.create({
@@ -7,19 +6,62 @@ const axiosInstance: AxiosInstance = axios.create({
   withCredentials: true,
 });
 
-// 요청 인터셉터 설정
 axiosInstance.interceptors.request.use(
-  function (config) {
-    // 토큰을 localStorage에서 가져옴
+  function(config: InternalAxiosRequestConfig) {
+    // headers가 undefined일 가능성에 대비한 코드 제거
+
     const token: string | null = localStorage.getItem('memberAccessToken');
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      // Authorization 헤더 설정 전 headers 객체가 존재하는지 확인
+      // 이제 headers 객체가 항상 초기화되어 있으므로, 직접 초기화할 필요 없음
+      config.headers['Authorization'] = `Bearer ${token}`;
     }
     return config;
   },
-  function (error) {
+  function(error) {
     return Promise.reject(error);
   }
 );
+
+// 응답 인터셉터 설정
+axiosInstance.interceptors.response.use(
+  function(response: AxiosResponse) {
+    return response;
+  },
+  async function(error) {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const accessToken = await refreshAccessToken();
+      // axios.defaults.headers.common 대신 axiosInstance의 인스턴스를 사용하여 헤더 설정
+      axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+      return axiosInstance(originalRequest);
+    }
+    return Promise.reject(error);
+  }
+);
+
+async function refreshAccessToken(): Promise<string> {
+  // 리프레시 토큰을 로컬 스토리지에서 가져옵니다.
+  const refreshToken: string | null = localStorage.getItem('memberRefreshToken');
+
+  console.log(refreshToken)
+
+  const response = await axios({
+    method: 'get',
+    url: `${process.env.REACT_APP_SPRINGBOOT_BASE_URL}/members/reissue`,
+    headers: {
+      Authorization: `Bearer ${refreshToken}` // 수정된 부분
+    },
+  });
+  
+  const newAccessToken = response.data.data.accessToken;
+
+  // 새로운 액세스 토큰을 로컬 스토리지에 저장합니다.
+  localStorage.setItem('memberAccessToken', newAccessToken);
+
+  return newAccessToken;
+}
+
 
 export default axiosInstance;
