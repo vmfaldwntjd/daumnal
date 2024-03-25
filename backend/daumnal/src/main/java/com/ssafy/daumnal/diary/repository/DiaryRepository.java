750746c9bf07c45e8f0d61518f1e9dcb3466886d
core <jinjs6@gmail.com>
2024-03-25 13:31:19 +0900

    feat: 캘린더 정보 조회 api 구현

--- backend/daumnal/src/main/java/com/ssafy/daumnal/diary/repository/DiaryRepository.java
+++ backend/daumnal/src/main/java/com/ssafy/daumnal/diary/repository/DiaryRepository.java
@@ -1,8 +1,11 @@
 package com.ssafy.daumnal.diary.repository;
 
+import com.ssafy.daumnal.diary.dto.nativedto.CalendarContent;
 import com.ssafy.daumnal.diary.entity.Diary;
 import com.ssafy.daumnal.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +16,10 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
     boolean existsByMember(Member member);
 
     List<Diary> findDiariesByMemberOrderByCreatedAtDesc(Member member);
+
+    @Query(value = "select diary.id as diaryId, music_id as musicId, greatest(angry, disgust, fear, happiness, neutral, sadness, surprise) as emotionFirst,\n" +
+            "       hash_tag as diaryHashTag, day(diary.created_at) as diaryDay from diary\n" +
+            "inner join emotion on diary.emotion_id = emotion.id\n" +
+            "where member_id = :memberId and year(diary.created_at) = :yearSql and month(diary.created_at) = :monthSql", nativeQuery = true)
+    List<CalendarContent> findDiariesByYearAndMonth(@Param("memberId") Long memberId, @Param("yearSql") int yearSql, @Param("monthSql") int monthSql);
 }
