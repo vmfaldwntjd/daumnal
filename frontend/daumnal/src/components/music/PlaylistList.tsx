d90a705f2f60cd0f90fc95973481b4e86ec38bc9
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-03-25 16:50:21 +0900

    feat: 플레이리스 생성, 플레이리스 목록 조회 API 통신 테스트

--- frontend/daumnal/src/components/music/PlaylistList.tsx
+++ frontend/daumnal/src/components/music/PlaylistList.tsx
@@ -1,5 +1,6 @@
 // 플레이리스트 목록 컴포넌트
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import axiosInstance from '../../pages/api/axiosInstance';
 import styled from 'styled-components';
 import CreatePlaylistModal from '../modal/CreatePlaylistModal';
 import PlaylistCard from './PlaylistCard';
@@ -8,26 +9,47 @@ interface PlaylistListProps {
   onPlaylistSelect: (playlistId: number) => void;
 }
 
+interface ApiResponse {
+  data: any;
+  code: number;
+  status: string;
+  message: string;
+}
+
+// Playlist 인터페이스 정의
+interface Playlist {
+  playlistId: number;
+  playlistName: string;
+  playlistCoverUrl: string;
+}
+
 const PlaylistList: React.FC<PlaylistListProps> = ({ onPlaylistSelect }) => {
   // 생성 모달 상태 변수
   const [isOpenCreateModal, setOpenCreateModal] = useState<boolean>(false);
+
+  const [playlists, setPlaylists] = useState<Playlist[]>([]);
 
   // 플레이리스트 생성 모달 열고 닫는 함수
   const handleCreatePlaylist = useCallback(() => {
     setOpenCreateModal(!isOpenCreateModal); // setOpenCreateModal 함수 호출하여 isOpenCreateModal 상태 토글
   }, [isOpenCreateModal]); // 배열에 있는 값들이 변경될 때에만 새로운 함수 생성
 
-  // 가상의 플레이리스트 데이터
-  const playlists = [
-    { playlistId: 1, playlistName: 'Playlist 1', playlistCoverUrl: null },
-    { playlistId: 2, playlistName: 'Playlist 2', playlistCoverUrl: null },
-    { playlistId: 3, playlistName: 'Playlist 3', playlistCoverUrl: null },
-    { playlistId: 4, playlistName: 'Playlist 4', playlistCoverUrl: null },
-    { playlistId: 5, playlistName: 'Playlist 5', playlistCoverUrl: null },
-    { playlistId: 6, playlistName: 'Playlist 6', playlistCoverUrl: null },
-    { playlistId: 7, playlistName: 'Playlist 7', playlistCoverUrl: null },
-    { playlistId: 8, playlistName: 'Playlist 8', playlistCoverUrl: null },
-  ]
+  // 플레이리스트 목록 요청
+  useEffect(() => {
+    axiosInstance.get<ApiResponse>(`${process.env.REACT_APP_SPRINGBOOT_BASE_URL}/playlists?pgno=#}`)
+      .then(response => {
+        console.log('플레이리스트 목록 요청 성공!', response.data);
+        if (response.data.code === 200) {
+          console.log(`${response.data.status}: ${response.data.message}`);
+          setPlaylists(response.data.data.content);
+        } else {
+          console.log(`${response.data.status}: ${response.data.message}`);
+        }
+      })
+      .catch(error => {
+        console.log('플레이리스트 목록 요청 오류 발생!', error);
+      });
+  }, [isOpenCreateModal]);
 
   return (
     <div className="flex flex-col items-center">
@@ -73,7 +95,7 @@ const Modal = styled.div`
 const Playlists = styled.div`
   width: 90%;
   height: 85vh;
-  overflow-y: auto; /* 세로 스크롤 추가 */
+  overflow-y: auto;
   background-color: #F8F6EE;
   display: flex;
   flex-wrap: wrap;
