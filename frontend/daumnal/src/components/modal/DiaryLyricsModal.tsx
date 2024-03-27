7d69603f920efbaa5914194f7d982a7367391443
jojoo <csh990720@gmail.com>
2024-03-27 16:22:36 +0900

    fix: 일기 조회 가사 모달 폰트 변경

--- frontend/daumnal/src/components/modal/DiaryLyricsModal.tsx
+++ frontend/daumnal/src/components/modal/DiaryLyricsModal.tsx
@@ -15,8 +15,9 @@ const DiaryLyricsModal: React.FC<DiaryLyricsModalProps> = ({ onClickToggleModal,
   const lyricsLines = diaryMusicLyrics.split('\n');
 
   return (
+    <div style={{ fontFamily: "NanumSquare" }}>
     <div className='fixed mr-[134px] inset-0 bg-white bg-opacity-0 flex justify-center items-center'> {/* 배경 클릭 시 모달 닫기 */}
-      <div onClick={(e) => e.stopPropagation()} className='w-[400px] h-[80%] m-auto  shadow-lg bg-white rounded-sm'> {/* 모달 컨텐츠 클릭 시 버블링 방지 */}
+      <div onClick={(e) => e.stopPropagation()} className='w-[420px] h-[80%] m-auto  shadow-lg bg-white rounded-sm'> {/* 모달 컨텐츠 클릭 시 버블링 방지 */}
         <div className='flex justify-end'><button onClick={onClickToggleModal} className='mt-2 mr-2 text-xl'><FontAwesomeIcon icon={faX} size="xs" style={{color: "#776b5d",}} /></button></div>
         <div className='h-[90%] overflow-y-auto flex flex-col items-center text-[#776B5D] font-[20px]'>
             {/* 가사 */}
@@ -25,6 +26,7 @@ const DiaryLyricsModal: React.FC<DiaryLyricsModalProps> = ({ onClickToggleModal,
             ))}
         </div>
       </div>
+    </div>
     </div>
   );
 };
