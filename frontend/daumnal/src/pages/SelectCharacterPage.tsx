dbc87bf165da9fd8fc7389efb8576750deb4e08b
Son Jieun <116146923+sje08250@users.noreply.github.com>
2024-04-01 17:32:56 +0900

    feat: 캐릭터 선택 페이지 키워드 추가

--- frontend/daumnal/src/pages/SelectCharacterPage.tsx
+++ frontend/daumnal/src/pages/SelectCharacterPage.tsx
@@ -7,13 +7,13 @@ const SelectCharacterPage: React.FC = () => {
 
   return (
     <div className="p-9">
-      <p className="text-4xl mb-7">오늘의 나무를 골라 주세요!</p>
+      <p className="text-5xl mb-9 mt-3 ml-5">오늘의 나무를 골라 주세요!</p>
       {/* 캐릭터 목록 */}
       <Characters>
-        <CharacterCard imageUrl="/image/dabom.png" name="다봄" category="SPRING" context="다봄이 설명" />
-        <CharacterCard imageUrl="/image/dareum.png" name="다름" category="SUMMER" context="다름이 설명" />
-        <CharacterCard imageUrl="/image/daeul.png" name="다을" category="FALL" context="다을이 설명" />
-        <CharacterCard imageUrl="/image/daseol.png" name="다설" category="WINTER" context="다설이 설명" />
+        <CharacterCard imageUrl="/image/dabom.png" name="다봄" category="SPRING" keywords={["#밝음", "#신나요", "#OST_좋아"]} />
+        <CharacterCard imageUrl="/image/dareum.png" name="다름" category="SUMMER" keywords={["#청량", "#즐거움", "#내적_댄스"]} />
+        <CharacterCard imageUrl="/image/daeul.png" name="다을" category="FALL" keywords={["#새벽", "#힙하다", "#감성_러버"]} />
+        <CharacterCard imageUrl="/image/daseol.png" name="다설" category="WINTER" keywords={["#포근", "#잔잔함", "#위로가_돼"]} />
       </Characters>
     </div>
   );
@@ -21,7 +21,7 @@ const SelectCharacterPage: React.FC = () => {
 
 const Characters = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-evenly;
 `;
 
 export default SelectCharacterPage;
